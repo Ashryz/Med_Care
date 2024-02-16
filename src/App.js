1d2600362d2f52a9677559f73f6e8a35f3f78695
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-02-16 09:53:51 +0200

    adding simple Home page

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SelectReg from './Components/SignUp/utils/pages/selectReg.js';
 import SignIn from './Pages/SignIn/SignIn.js'
-import { Outlet } from "react-router-dom";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ForgotPassword from './Pages/SignIn/ForgotPassword.js';
-import Userprofile from './Components/UserProfile/Userprofile'; 
+import Userprofile from './Components/UserProfile/Userprofile';
 import ChangePass from './Components/ChangePass/ChangePass';
+import Home from './Pages/SignIn/Home.js';
 
 
 
 function App() {
   return (
     <div className="App">
-   
+
       <BrowserRouter>
-      
+
         <Routes>
-        
-          <Route path='/ForgotPassword' element={<ForgotPassword/>}/>
-          <Route path='/SignIn' element={<SignIn/>}/>
-          <Route path='/SignUp' element={<SelectReg/>}/>
-          <Route exact path="/Userprofile" element={<Userprofile/>} />
-          <Route exact path="/changepassword" element={<ChangePass/>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/ForgotPassword' element={<ForgotPassword />} />
+          <Route path='/SignIn' element={<SignIn />} />
+          <Route path='/SignUp' element={<SelectReg />} />
+          <Route exact path="/Userprofile" element={<Userprofile />} />
+          <Route exact path="/changepassword" element={<ChangePass />} />
         </Routes>
       </BrowserRouter>
       {/* <SelectReg/> */}
