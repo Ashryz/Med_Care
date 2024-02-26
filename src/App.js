ca531a52c7b3dd5dd6b968a63ff11a09a8132f4c
Yusuf Abu-Alam <yabualam@gmail.com>
2024-02-26 12:09:41 +0200

    Merge searchbar with main

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ import DoctorProfile from './Components/DoctorProfile/DoctorProfile';
 import Hero from './Components/Hero/Hero.js';
 import FooterComp from './Components/Footer/Footer.js';
 import Error from './Components/Error/Error.js';
-import SearchResults from './Components/Search/SearchResults.js';import Review from './Pages/review/Review.js';
+import SearchResults from './Components/Search/SearchResults.js';
+import Review from './Pages/review/Review.js';
 
 function App() {
   return (
@@ -30,6 +31,7 @@ function App() {
           <Route exact path="/changepassword" element={<ChangePass />} />
           <Route exact path="/Listdoctor" element={<ListDoctor />} />
           <Route exact path="/DoctorProfile" element={<DoctorProfile />} />
+          <Route exact path="/SearchResults" element={<SearchResults />} />
           <Route exact path="/Hero" element={<Hero />} />
           <Route exact path='/review' element={<Review />} />
           <Route path='*' element={<Error />} />
