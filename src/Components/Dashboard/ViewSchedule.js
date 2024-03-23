94b0c7c1ec2efd3cd28aca93705753840dc761d6
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-03-23 20:04:46 +0200

    redirect done

--- src/Components/Dashboard/ViewSchedule.js
+++ src/Components/Dashboard/ViewSchedule.js
@@ -1,58 +1,77 @@
-import React, { useEffect, useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import Sidebar from './Sidebar';
-import './style.css';
-import SchadulesCard from '../ViewAppointment/SchedulesCard';
-import { axiosInstance } from '../../Network/axiosInstance';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import Sidebar from "./Sidebar";
+import "./style.css";
+import SchadulesCard from "../ViewAppointment/SchedulesCard";
+import { axiosInstance } from "../../Network/axiosInstance";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 function ViewSchedule() {
-    const [schedules, setschedules] = useState([])
-    const [refresh, setRefresh] = useState(false);
-    useEffect(() => {
-        const userId = JSON.parse(localStorage.getItem("user")).id;
-        axiosInstance.get(`/schedules/all_sch/doctor/${userId}/`)
-            .then((res) => {
-                setschedules(res.data);
-                console.log(res)
-            })
-            .catch((err) => console.log(err))
-    }, [refresh])
-    console.log(schedules.Results)
-    return (
-        <>
-            <div className="App"
-                style={{ minHeight: "75vh" }}>
+  const [schedules, setschedules] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+  const authContext = useContext(AuthContext);
+  const currentUser = authContext.currentUser;
+  const navigate = useNavigate();
 
-                <div className="container-fluid">
-                    <div className="row">
-                        <hr />
-                        <div className="side col-md-3">
-                            <Sidebar />
-                        </div>
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/");
+    }
+  }, [currentUser, navigate]);
 
-                        <div className="col-md-9 mt-3">
-                            {schedules.length === 0 ? (
-                                <div className="text-center">
-                                    <h1 className="text-muted"><i className="bi bi-calendar me-2"></i><br/>No Schedules Found</h1>
-                                    <hr className="w-75 mx-auto sec-color shadow rounded-5" />
-                                </div>
-                            ) : (
-                                <div className='row d-flex'>
-                                    {schedules.map((schedule) => (
-                                        <div key={schedule.id} className='col-md-3'>
-                                            <SchadulesCard schedule={schedule} refresh={refresh} setRefresh={setRefresh} />
-                                        </div>
-                                    ))}
-                                </div>
-                            )}
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    const userId = currentUser.id;
+    axiosInstance
+      .get(`/schedules/all_sch/doctor/${userId}/`)
+      .then((res) => {
+        setschedules(res.data);
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  }, [refresh, currentUser]);
+
+  return (
+    <>
+      <div className="App" style={{ minHeight: "75vh" }}>
+        <div className="container-fluid">
+          <div className="row">
+            <hr />
+            <div className="side col-md-3">
+              <Sidebar />
             </div>
 
-        </>
-    );
+            <div className="col-md-9 mt-3">
+              {schedules.length === 0 ? (
+                <div className="text-center">
+                  <h1 className="text-muted">
+                    <i className="bi bi-calendar me-2"></i>
+                    <br />
+                    No Schedules Found
+                  </h1>
+                  <hr className="w-75 mx-auto sec-color shadow rounded-5" />
+                </div>
+              ) : (
+                <div className="row d-flex">
+                  {schedules.map((schedule) => (
+                    <div key={schedule.id} className="col-md-3">
+                      <SchadulesCard
+                        schedule={schedule}
+                        refresh={refresh}
+                        setRefresh={setRefresh}
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default ViewSchedule;
