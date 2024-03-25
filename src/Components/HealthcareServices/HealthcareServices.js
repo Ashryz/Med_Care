8ecdd1d13a47c54d89906e765bb4c5226a9679fa
Israa Lotfy <israalotfy891@gmail.com>
2024-03-26 00:11:31 +0200

    disable btn for doctor

--- src/Components/HealthcareServices/HealthcareServices.js
+++ src/Components/HealthcareServices/HealthcareServices.js
@@ -1,14 +1,16 @@
-import React, { useState ,useContext} from "react";
+import React, { useState, useContext } from "react";
 import { useSelector } from "react-redux";
-import axios from "axios"; 
+import axios from "axios";
 import { axiosInstance } from "../../Network/axiosInstance";
 import { AuthContext } from "../../context/AuthContext";
+
 function HealthcareServices() {
   const [showModal, setShowModal] = useState(false);
   const [showSuccessMessage, setShowSuccessMessage] = useState(false);
   const [question, setQuestion] = useState(""); // State to store the question
   const authContext = useContext(AuthContext);
   const localuser = JSON.parse(localStorage.getItem("user"));
+  const isDoctor = localuser && localuser.is_doctor; 
   const mytheme = useSelector((state) => state.combineThemes.theme);
 
   const handleAskNow = () => {
@@ -19,16 +21,15 @@ function HealthcareServices() {
     setShowModal(false);
   };
 
-  const handleSubmit = async (event) => { 
+  const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      
       const response = await axiosInstance.post(`questions/user/${localuser.id}/`, { question });
       setShowSuccessMessage(true);
       setQuestion(""); // Clear the question input
-          setTimeout(() => {
-          setShowModal(false);
-        }, 1000);
+      setTimeout(() => {
+        setShowModal(false);
+      }, 1000);
     } catch (error) {
       console.error("Error submitting question:", error);
     }
@@ -45,18 +46,15 @@ function HealthcareServices() {
         <div className="col-12">
           <div className="row mb-4">
             <div className="col-12">
-              <div className={`card service-card border p-3 rounded-3 shadow ${mytheme === 'light'? '':'text-white bg-dark'} `}>
+              <div className={`card service-card border p-3 rounded-3 shadow ${mytheme === 'light' ? '' : 'text-white bg-dark'} `}>
                 <div className="card-body m-3">
-                  <h5 className="card-title prim-color service-title">
-                    Have a Medical Question?
-                  </h5>
-                  <p className="card-text service-text ">
-                    Submit your medical question and receive an answer from a
-                    specialized doctor.
-                  </p>
-                  <button className="btn main-btn" onClick={handleAskNow}>
-                    Ask now
-                  </button>
+                  <h5 className="card-title prim-color service-title">Have a Medical Question?</h5>
+                  <p className="card-text service-text ">Submit your medical question and receive an answer from a specialized doctor.</p>
+                  {!isDoctor && (
+                    <button className="btn main-btn" onClick={handleAskNow} disabled={isDoctor}>
+                      Ask now
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
@@ -69,33 +67,18 @@ function HealthcareServices() {
         <div className=" modal fade show" style={{ display: "block" }}>
           <div className="modal-dialog modal-dialog-centered  ">
             <div className="modal-content border rounded-3 opacity-1 shadow  ">
-              <div className={`modal-header ${mytheme === 'light'? '':'text-white bg-dark'} `}>
+              <div className={`modal-header ${mytheme === 'light' ? '' : 'text-white bg-dark'} `}>
                 <h5 className="modal-title">Ask a Medical Question</h5>
-                <button
-                  type="button"
-                  className={`btn-close  ${mytheme === 'light'? '':'bg-white '} `}
-                  onClick={handleCloseModal}
-                ></button>
+                <button type="button" className={`btn-close  ${mytheme === 'light' ? '' : 'bg-white '} `} onClick={handleCloseModal}></button>
               </div>
-              <div className={`modal-body ${mytheme === 'light'? '':'text-white bg-dark'} `}>
+              <div className={`modal-body ${mytheme === 'light' ? '' : 'text-white bg-dark'} `}>
                 <form onSubmit={handleSubmit}>
                   <div className="mb-3">
-                    <label htmlFor="question" className="form-label">
-                      Your Question
-                    </label>
-                    <textarea
-                      className="form-control"
-                      id="question"
-                      rows="3"
-                      value={question}
-                      onChange={(e) => setQuestion(e.target.value)}
-                      required
-                    ></textarea>
+                    <label htmlFor="question" className="form-label">Your Question</label>
+                    <textarea className="form-control" id="question" rows="3" value={question} onChange={(e) => setQuestion(e.target.value)} required></textarea>
                   </div>
-                  <div className={`modal-footer  ${mytheme === 'light'? '':'text-white bg-dark'}`}>
-                    <button type="submit" className="btn main-btn">
-                      Submit
-                    </button>
+                  <div className={`modal-footer  ${mytheme === 'light' ? '' : 'text-white bg-dark'}`}>
+                    <button type="submit" className="btn main-btn">Submit</button>
                   </div>
                 </form>
               </div>
